7770e39f8c84afb7e0e871e75aa7df1ccc4e3da8
Ashleylegit <99674466+Ashleylegit@users.noreply.github.com>
2022-06-08 19:06:27 -0600

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,43 +1,18 @@
-<<<<<<< HEAD
-#include "holberton.h"
-
-/**
- * print_alphabet - Entry point
- * Return:0
- */
-
-void print_alphabet(void)
-{
-	char c = 'a';
-
-	while (c <= 'z')
-	{
-		_putchar(c);
-		c++;
-
-	}
-	_putchar('\n');
-
-}
-=======
 #include "main.h"
 
 /**
- * print_alphabet - Entry point
- * Return:0
+ * print_alphabet - print alphabet
+ *
+ * Return: void
  */
-
 void print_alphabet(void)
 {
-	char c = 'a';
+	char letter = 'a';
 
-	while (c <= 'z')
+	while (letter <= 'z')
 	{
-		_putchar(c);
-		c++;
-
+		_putchar(letter);
+		letter++;
 	}
 	_putchar('\n');
-
 }
->>>>>>> 4570113193b0c71f131d832d0e9ccb637f3c2249
