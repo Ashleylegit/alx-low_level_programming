e95c90659d52e6e9124acdc2adcabe58fa11c99f
Ashleylegit <ashleybk876@gmail.com>
2022-06-16 20:04:53 -0600

    pointers arrays and strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,22 +1,28 @@
 #include "main.h"
+
 /**
- * *_strcat - concatenates two strings
- * @dest: pointer destination
- * @src: pointer source
- * Return: void
+*_strcat - concatenates two strings
+*@dest: string
+*@src: string
+*Return: pointer to dest
 */
 
 char *_strcat(char *dest, char *src)
 {
-int a = -1, i;
-for (i = 0; dest[i] != '\0'; i++)
-;
+int i, j;
 
-do {
-	a++;
-	dest[i] = src[a];
-	i++;
-} while (src[a] != '\0');
+i = 0;
+while (dest[i] != '\0')
+{
+i++;
+}
+
+for (j = 0; src[j] != '\0'; j++)
+{
+dest[i + j] = src[j];
+}
+
+dest[i + j] = '\0';
 
 return (dest);
 }
--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,26 +1,22 @@
 #include "main.h"
+
 /**
- * _strcmp - compares two strings
- * @s1: string 1
- * @s2: string 2
- * Return: 0 if matching, and ns1 - ns2 if not matching
+* _strcmp - compares two strings
+*@s1: string
+*@s2: string
+*Return: positive or negative or 0
 */
 
 int _strcmp(char *s1, char *s2)
 {
-int i;
-
-/**
- * We have reached the end of S1 OR we have reached the end of S2
- * 
-*/
-
-for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+while (*s1)
 {
-	if (s1[i] != s2[i])
-	{
-		return (s1[i] - s2[i]);
-	}
+if (*s1 != *s2)
+{
+break;
 }
-return (0);
+s1++;
+s2++;
+}
+return (*s1 - *s2);
 }
